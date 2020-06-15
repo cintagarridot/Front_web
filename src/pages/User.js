d40a6520365ddcb1bccaec598c24772ac35d60f7
cintagarridot <cinta_gt96@hotmail.com>
2020-06-15 17:55:56 +0200

    user page design changed

--- src/pages/User.js
+++ src/pages/User.js
@@ -22,25 +22,47 @@ class User extends Component {
                 <h2 className="subheaderUser">Datos</h2>
 
                 {/*} <div id="imgUsuario">*/}
-                <Row>
-                    <Col xs={'3'} style={{marginRight: '20px'}}>
+                <Row className={'mb-5 pb-5'}>
+                    <Col xs={'3'} style={{ marginRight: '20px' }}>
                         <img src={logo} class="app-logo" alt="Logotipo" />
                     </Col>
                     <Col xs={'5'}>
-                        <h3>Nombre: {this.props.user.firstName}</h3>
-                        <h3>Apellidos: {this.props.user.lastName}</h3>
-                        <h3>Username: {this.props.user.username}</h3>
+                        <Row>
+                            <Col xs={'12'} className={'pb-3'}>
+                                <h3 className='profile-text'>Nombre</h3>
+                                <h3>{this.props.user.firstName}</h3>
+                            </Col>
+                            <Col xs={'12'} className={'pb-3'}>
+                                <h3 className='profile-text'>Apellidos</h3>
+                                <h3>{this.props.user.lastName}</h3>
+                            </Col>
+                            <Col xs={'12'} className={'pb-3'}>
+                                <h3 className='profile-text'>Username</h3>
+                                <h3>{this.props.user.username}</h3>
+                            </Col>
+                            <Col xs={'12'}>
+                                <h3 className='profile-text'>Número de asignaturas matriculadas</h3>
+                                <h3>{this.props.user.subjects.length}</h3>
+                            </Col>
+                        </Row>
+
+
+                    </Col>
+                </Row>
+
+                <Row className={'mt-5'}>
+                    <Col xs={'2'} className={'mt-5 mr-3'}>
+                        <button style={{fontSize: '12px'}} >Cambiar contraseña</button>
+                    </Col>
+                    <Col xs={'2'} className={'mt-5'}>
+                        <button style={{fontSize: '12px'}} onClick={this.props.logout}>Cerrar sesión</button>
+
                     </Col>
                 </Row>
 
 
-
-
                 <div className="clearfix"></div>
-                <div id="botonUser">
-                    <button className="datosUser">Cambiar contraseña</button><br />
-                    <button className="datosUser" onClick={this.props.logout}>Cerrar sesión</button>
-                </div>
+
 
 
             </div>
