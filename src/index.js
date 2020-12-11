1f7b7abda412f7e3c85a041051427953df5008a2
Cinta <cgarrido@emergya.com>
2020-12-11 17:39:31 +0100

    header and datalistView colors and position

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-document.body.style = "background: #FFFFFF;";
+document.body.style = "background: #f7f7f7d7;";
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
