2adfc7b47128b43681c4d3848d216f3ed6f37261
Cinta <cgarrido@emergya.com>
2021-01-01 19:22:30 +0100

    Updates

---
+++ precache-manifest.bd8d2e62a1ff95e6fe3034b99568f80d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161dd6bb4e8c9f20ecca52069cfe84a9",
+    "url": "/Front_web/index.html"
+  },
+  {
+    "revision": "c1dae6f45136f04b5354",
+    "url": "/Front_web/static/css/2.44e6c743.chunk.css"
+  },
+  {
+    "revision": "249b643344dc0769909e",
+    "url": "/Front_web/static/css/main.e10b3983.chunk.css"
+  },
+  {
+    "revision": "c1dae6f45136f04b5354",
+    "url": "/Front_web/static/js/2.3da4523d.chunk.js"
+  },
+  {
+    "revision": "e50e5283c4cc2c4cec84",
+    "url": "/Front_web/static/js/3.1605c06d.chunk.js"
+  },
+  {
+    "revision": "3ea015e4f473536e0520",
+    "url": "/Front_web/static/js/4.06c7c4a1.chunk.js"
+  },
+  {
+    "revision": "66683bdb01495e85f6d9",
+    "url": "/Front_web/static/js/5.d5550c31.chunk.js"
+  },
+  {
+    "revision": "249b643344dc0769909e",
+    "url": "/Front_web/static/js/main.bd091d2e.chunk.js"
+  },
+  {
+    "revision": "261b37083ee6cca3b641",
+    "url": "/Front_web/static/js/runtime~main.cac999d1.js"
+  },
+  {
+    "revision": "41360347c336dd68bb4d02d8cdaba199",
+    "url": "/Front_web/static/media/loading.41360347.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Front_web/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "6273d90afce61acec1cf699bde2ff2f6",
+    "url": "/Front_web/static/media/logoetsi.6273d90a.png"
+  },
+  {
+    "revision": "73c6000e89d3af023573659dadbfc91d",
+    "url": "/Front_web/static/media/ojoVerNoticia.73c6000e.png"
+  }
+]);
