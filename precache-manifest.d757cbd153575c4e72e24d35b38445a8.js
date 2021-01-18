04556fe5caeef029494ce72aec91a0a1a0065ab3
Cinta <cgarrido@emergya.com>
2021-01-18 19:39:39 +0100

    Updates

--- precache-manifest.d757cbd153575c4e72e24d35b38445a8.js
+++ precache-manifest.d757cbd153575c4e72e24d35b38445a8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2c29084a4be160889340e81ed1ce7ffd",
+    "revision": "2339f14ab82a8b7540278d4ccfca713b",
     "url": "/Front_web/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Front_web/static/css/2.44e6c743.chunk.css"
   },
   {
-    "revision": "d60bfd999704e750cb2f",
+    "revision": "cc5eb94b90b21947e750",
     "url": "/Front_web/static/css/main.e82aff72.chunk.css"
   },
   {
@@ -28,8 +28,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Front_web/static/js/5.d5550c31.chunk.js"
   },
   {
-    "revision": "d60bfd999704e750cb2f",
-    "url": "/Front_web/static/js/main.8b26b821.chunk.js"
+    "revision": "cc5eb94b90b21947e750",
+    "url": "/Front_web/static/js/main.39454a65.chunk.js"
   },
   {
     "revision": "49da92d42e5e17018e8a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Front_web/precache-manifest.833d45f0aa414c49070f79025a6f264f.js"
+  "/Front_web/precache-manifest.d757cbd153575c4e72e24d35b38445a8.js"
 );
 
 self.addEventListener('message', (event) => {
