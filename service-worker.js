fa2d400b47e8d0604c578de5bad3cc4d5138d4b0
Cinta <cgarrido@emergya.com>
2021-01-29 16:52:57 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Front_web/precache-manifest.37e2ce284bdbef46569a647a0158a994.js"
+  "/Front_web/precache-manifest.d333bbed9afdce4929da8ae4409175ee.js"
 );
 
 self.addEventListener('message', (event) => {
