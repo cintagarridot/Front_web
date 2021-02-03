86d5c6c5748127c72addb961d35cf890b21ae69d
Cinta <cgarrido@emergya.com>
2021-02-03 18:07:03 +0100

    Updates

---
+++ precache-manifest.ef6ced92ecf7b2b71bb204396973ecac.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1aa644e6ddc80c6dbd662707ea54049",
+    "url": "/Front_web/index.html"
+  },
+  {
+    "revision": "c7016dd4480009c833f5",
+    "url": "/Front_web/static/css/2.44e6c743.chunk.css"
+  },
+  {
+    "revision": "92753640fde4461918f4",
+    "url": "/Front_web/static/css/main.c215ac16.chunk.css"
+  },
+  {
+    "revision": "c7016dd4480009c833f5",
+    "url": "/Front_web/static/js/2.a2bdac06.chunk.js"
+  },
+  {
+    "revision": "0bb432b89f53a829d02d",
+    "url": "/Front_web/static/js/3.fa7c9fa7.chunk.js"
+  },
+  {
+    "revision": "4d57a89f3e393952bc87",
+    "url": "/Front_web/static/js/4.db593955.chunk.js"
+  },
+  {
+    "revision": "f5bfc04c455b72990f84",
+    "url": "/Front_web/static/js/5.6d9fb266.chunk.js"
+  },
+  {
+    "revision": "92753640fde4461918f4",
+    "url": "/Front_web/static/js/main.9f3adc68.chunk.js"
+  },
+  {
+    "revision": "d056d26f34a13fa74113",
+    "url": "/Front_web/static/js/runtime~main.61e64a2a.js"
+  },
+  {
+    "revision": "f8f44493be7219ca411a1aac430d0d0f",
+    "url": "/Front_web/static/media/UHU.f8f44493.png"
+  },
+  {
+    "revision": "50d633d2b8791fe4312beb1eef5380c2",
+    "url": "/Front_web/static/media/asignaturas.50d633d2.png"
+  },
+  {
+    "revision": "abb7ec5835591606709c11a2a0beca3e",
+    "url": "/Front_web/static/media/documentos.abb7ec58.jpg"
+  },
+  {
+    "revision": "41360347c336dd68bb4d02d8cdaba199",
+    "url": "/Front_web/static/media/loading.41360347.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Front_web/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "6273d90afce61acec1cf699bde2ff2f6",
+    "url": "/Front_web/static/media/logoetsi.6273d90a.png"
+  },
+  {
+    "revision": "73c6000e89d3af023573659dadbfc91d",
+    "url": "/Front_web/static/media/ojoVerNoticia.73c6000e.png"
+  },
+  {
+    "revision": "c264eae757b98439ec4087ff8c857de1",
+    "url": "/Front_web/static/media/uhu.c264eae7.jpg"
+  },
+  {
+    "revision": "a9fb1443c4a6dda411670d6c0fd90707",
+    "url": "/Front_web/static/media/usuario.a9fb1443.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Front_web/precache-manifest.d3356ae9b8aae2bc7386d6cfc73f7410.js"
+  "/Front_web/precache-manifest.ef6ced92ecf7b2b71bb204396973ecac.js"
 );
 
 self.addEventListener('message', (event) => {
