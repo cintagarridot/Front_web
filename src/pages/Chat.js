0299598886fd100284745e72d686391951f9d9c9
Cinta <cgarrido@emergya.com>
2021-02-21 16:49:57 +0100

    chat socket cors

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -42,17 +42,16 @@ const Chat = ({user, ...props}) => {
     //     }
     // }))
 
-    const [socket] = useState(io(URL
-        // , {
-        // withCredentials: true,
-        // transportOptions: {
-        //     polling: {
-        //         extraHeaders: {
-        //             "Access-Control-Allow-Headers": "Bearer test, Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
-        //         }
-        //     }
-        // }
-    /*}*/));
+    const [socket] = useState(io(URL, {
+        withCredentials: true,
+        transportOptions: {
+            polling: {
+                extraHeaders: {
+                    "Access-Control-Allow-Headers": "Bearer test, Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+                }
+            }
+        }
+    }));
 
     console.log('socket after useState', socket);
 
