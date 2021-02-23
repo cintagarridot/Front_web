22cd8627c3b61900b67692e269b577df9cdb30a7
Cinta <cgarrido@emergya.com>
2021-02-23 19:51:25 +0100

    list chat username

--- src/pages/ListChats.js
+++ src/pages/ListChats.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
 import chatService from 'services/chat-service';
