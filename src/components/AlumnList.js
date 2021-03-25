b22bd68fcd2523387257c60f4914d113a64a8f23
Cinta <cgarrido@emergya.com>
2021-03-25 20:41:14 +0100

    leyenda en la lista de alumnos

--- src/components/AlumnList.js
+++ src/components/AlumnList.js
@@ -40,16 +40,30 @@ class AlumnList extends Component {
                 {this.state.status !== 'success' &&
                     <Spinner color="info" />
                 }
-                {this.state.alumns && this.state.alumns.length > 0 && this.state.status === 'success'  && this.state.alumns.map(a => {
-                     return (
-                        <DataListView
-                            key={a.id}
-                            element={a}
-                            onCheckItem={this.onCheckItem}
-                            usersList   
-                        />
-                     )
-                })}
+                {this.state.alumns && this.state.alumns.length > 0 && this.state.status === 'success'  && (
+                    <>
+                        <Row className="d-flex justify-content-center mb-1 mt-1">
+                            <Col xs={'6'} sm={'6'} lg={'6'} xl={'6'}>
+                                <p className="list-item-heading mb-1 truncate">
+                                    NOMBRE
+                                </p>
+                            </Col>
+                            <Col xs={'6'} sm={'6'} lg={'6'} xl={'6'}>
+                                <p className="list-item-heading mb-1 truncate">
+                                    USUARIO
+                                </p>
+                            </Col>
+                        </Row>
+                        {this.state.alumns.map(a => {
+                            return <DataListView
+                                key={a.id}
+                                element={a}
+                                onCheckItem={this.onCheckItem}
+                                usersList   
+                            />
+                        })}
+                    </>
+                )}
                  {this.state.alumns && this.state.alumns.length === 0 && this.state.status === 'success' &&
 
                     <h2 className="text-center">No hay alumnos registrados</h2>
